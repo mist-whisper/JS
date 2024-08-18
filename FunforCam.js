e2b28bb576ba19779b99d48305dc3797cae6d0b3
mist-whisper <114045385+mist-whisper@users.noreply.github.com>
2024-08-18 21:41:23 +0800

    Update FunforCam.js

--- FunforCam.js
+++ FunforCam.js
@@ -12,8 +12,8 @@
 使用声明：此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖！
 *******************************
 [rewrite_local]
-# >方弗相机（恢复购买）
-^https?:\/\/api.revenuecat.com\/v1\/(receipts|subscribers\/\$RCAnonymousID) url script-response-body https://raw.githubusercontent.com/WeiGiegie/666/main/ffxj.js
+# >方弗相机
+^https?:\/\/api.revenuecat.com\/v1\/(receipts|subscribers\/\$RCAnonymousID) url script-response-body https://raw.githubusercontent.com/mist-whisper/JS/master/FunforCam.js
 
 [mitm] 
 hostname = api.revenuecat.com
