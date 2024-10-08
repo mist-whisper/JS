f673761479b53ff3e6dbc11df9b0870ef236e627
mist-whisper <114045385+mist-whisper@users.noreply.github.com>
2024-10-08 16:38:18 +0800

    Create javdb.js

---
+++ webpage/javdb.js
@@ -0,0 +1,12 @@
+// https://github.com/RuCu6/Loon/blob/main/Scripts/webpage/javdb.js
+// 2024-06-30 11:40
+
+if (!/html>/.test($response.body)) $done({});
+
+// 去顶部域名,底部下载提醒,播放页广告
+const body = $response.body.replace(
+  /<\/head>/,
+  "<style> .sub-header, .app-desktop-banner, .moj-content {display:none!important;} </style> \n </head>"
+);
+
+$done({ body });
