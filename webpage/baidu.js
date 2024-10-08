629b58901821d6432b6828d72f0900b13c5a1b93
mist-whisper <114045385+mist-whisper@users.noreply.github.com>
2024-10-08 17:14:08 +0800

    Delete webpage/baidu.js

--- webpage/baidu.js
+++
@@ -1,7 +0,0 @@
-// https://github.com/RuCu6/Loon/blob/main/Scripts/webpage/baidu.js
-
-let rHead = "<head>";
-let newStyle =
-  "<head><style> .wpoScript, .ec_ad_results, .ec_wise_ad, .page-copyright, .c-line-clamp1{display:none!important} </style>";
-let body = $response.body.replace(rHead, newStyle);
-$done({ body });
